89f1eec5ee4ef503aa33f56417d275726c0ced75
Henry Filosa <hfilosa@gmail.com>
2016-04-21 09:54:51 -0400

    waT

--- balls.js
+++ balls.js
@@ -1,19 +1,54 @@
 var ball = function() {
-    var x=0;
-    var y=0;
+    var x=250;
+    var y=250;
+    var vel=1;
+    var theta=0
+    var radius=10;
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx",x);
+    c.setAttribute("cy",y);
+    c.setAttribute("r",radius);
+    c.setAttribute("fill","red");
+    c.setAttribute("stroke","black");
+    document.getElementById("vimage").appendChild(c);
     var getX = function(){
 	return x;
     };
     var getY = function(){
-	return x;
+	return y;
     };
     var set = function(x,y){
-	this.x=x;
-	this.y=y;
+	c.setAttribute("cx",250);
+	c.setAttribute("cy",250);
+    };
+    var getTheta = function(){
+	return theta;
+    };
+    var getVel = function(){
+	return vel;
+    };
+    var getRadius = function(){
+	return Radius;
+    };
+    var setVel = function(vel,theta){
+	this.vel=vel;
+	this.theta=theta;
+    };
+    var move = function(){
+	c.setAttribute("cx",c.getAttribute("cx")+vel*cos(theta));
+	c.setAttribute("cy",c.getAttribute("cy")+vel*sin(theta));
     }
     return {
 	getX: getX,
 	getY: getY,
-	set: set
+	set: set,
+	getVel: getVel,
+	setVel: setVel,
+	getRadius: getRadius,
+	move: move
     }
 }
+
+var balls = []
+ball1=ball
+balls.push(ball1)
