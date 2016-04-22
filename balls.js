8351e461f3333e64cbaf37ad5bad28277ecc663a
Henry Filosa <hfilosa@gmail.com>
2016-04-22 10:01:35 -0400

    Used map to increase and decrease speed

--- balls.js
+++ balls.js
@@ -41,53 +41,47 @@ var ball = function() {
 	return Radius;
     };
     var setVel = function(n){
-	vel+=n;
+	if (vel+n <0)
+	    vel=0;
+	else
+	    vel+=n;
     };
     var move = function(){
-	console.log("theta "+theta);
 	theta=theta%360;
-	if (checked!=0)
-	    checked-=1;
 	new_x=x+vel*Math.cos((theta/180.0)*Math.PI);
 	new_y=y+vel*Math.sin((theta/180.0)*Math.PI);
-	if (checked==0){
         if (theta >= 0 && theta < 90){ //quadrant 1
-            if (new_x + radius > width){
+	    if (new_x + radius > width){
                 theta = 180 - theta;
-            }
-            else if (new_y + radius > height){
+	    }
+	    else if (new_y + radius > height){
                 theta = 360 - theta;
-            }
-            checked = 2
+	    }
         }
         else if (theta >= 90 && theta < 180){ //quadrant 2
-            if (new_x - radius < 0){
+	    if (new_x - radius < 0){
                 theta = 180 - theta;
-            }
-            else if (new_y + radius > height){
+	    }
+	    else if (new_y + radius > height){
                 theta = 360 - theta;
-            }
-            checked = 2
+	    }
         }
         else if (theta >= 180 && theta < 270){ //quadrant 3
-            if (new_x - radius < 0){
+	    if (new_x - radius < 0){
                 theta = (540 - theta) % 360;
-            }
-            else if (new_y - radius < 0){
+	    }
+	    else if (new_y - radius < 0){
                 theta = 360 - theta;
-            }
-            checked = 2
+	    }
         }
         else if (theta >= 270 && theta < 360){ //quadrant 4
-            if (new_x + radius > width){
+	    if (new_x + radius > width){
                 theta = (540 - theta) % 360;
-            }
-            else if (new_y - radius < 0){
+	    }
+	    else if (new_y - radius < 0){
                 theta = 360 - theta;
-            }
-            checked = 2
+	    }
         }
-	}
 	x+=vel*Math.cos((theta/180.)*Math.PI);
 	y+=vel*Math.sin((theta/180.)*Math.PI);
 	c.setAttribute("cx",x);
@@ -112,39 +106,7 @@ var balls = []
 
 Id=window.setInterval(function(){for (var i=0;i<balls.length;i++) balls[i].move();},16)
 
-document.getElementById("inc").addEventListener("click", function(){for (var i = 0; i < balls.length; i++) balls[i].setVel(1);});
-document.getElementById("dec").addEventListener("click", function(){for (var i = 0; i < balls.length; i++) balls[i].setVel(-1);});
-document.getElementById("add").addEventListener("click", function(){ balls.push(ball());  });
-document.getElementById("rem").addEventListener("click", function(){balls[balls.length-1].remove();balls.pop() });
-
-
-/*
-if (new_x+radius>width){
-		if (theta<180)
-		    theta-=90;
-		else
-		    theta+=90;
-		checked=2;
-	    }
-	    else if (new_x-radius<0){
-		if (theta<180)
-		    theta+=90;
-		else
-		    theta-=90;
-		checked=2;
-	    }
-	    else if (new_y+radius>height){
-		if (theta<180)
-		    theta-=90;
-		else
-		    theta+=90;
-		checked=2;
-	    }
-	    else if (new_y-radius<0){
-		if (theta<180)
-		    theta+=90;
-		else
-		    theta-=90;
-		checked=2;
-	    }
-*/
+document.getElementById("inc").addEventListener("click",function(){balls.map(function(ball){ball.setVel(1);}) });
+document.getElementById("dec").addEventListener("click",function(){balls.map(function(ball){ball.setVel(-1);}) });
+document.getElementById("add").addEventListener("click",function(){ balls.push(ball()); });
+document.getElementById("rem").addEventListener("click",function(){balls[balls.length-1].remove();balls.pop() });
